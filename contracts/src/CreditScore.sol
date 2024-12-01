23230d0275b137af051b0fd8e8ce2563abfa09b7
Reblixt <carl@klofverskjold.se>
2024-12-01 22:44:23 +0100

    had to uninstall Soldeer and created some psheduo code and started on create user and some other admin funcitons

--- contracts/src/CreditScore.sol
+++ contracts/src/CreditScore.sol
@@ -2,4 +2,179 @@
 
 pragma solidity ^0.8.28;
 
-contract CreditScore {}
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
+import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {ReentrancyGuardUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+
+contract CreditScore is
+    ReentrancyGuardUpgradeable,
+    AccessControlUpgradeable,
+    PausableUpgradeable,
+    OwnableUpgradeable
+{
+    // ================= Type Declarations =================
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant LENDER_ROLE = keccak256("BANK_ROLE");
+
+    struct PaymentPlan {
+        uint256 interestRate;
+        uint256 amount;
+        uint256 time;
+        uint256 paidDebt;
+        uint256 unpaidDebt;
+        uint256 totalPaid;
+        bool active;
+    }
+
+    mapping(address => uint256) private userMeanCreditScores;
+    mapping(address => uint256) private userNumberOfCreditScores;
+    mapping(address => PaymentPlan[]) public userPaymentPlans;
+    mapping(address => mapping(address => bool)) public userApprovedLender;
+    mapping(address => mapping(address => uint256))
+        public LenderUserCreditScore;
+
+    // ================= State Variables =================
+    address[] private activeLenders;
+
+    // ================= Events ==========================
+    event LenderAdded(address indexed lender);
+    event LenderRemoved(address indexed lender);
+    event LenderUpdated(address indexed lender, address indexed newLender);
+    event PaymentPlanPaid(address indexed taker);
+    event CreditScoreCreated(address indexed taker);
+    event PaymentPlanCreated(address indexed taker);
+    event CreditScoreUpdated(address indexed taker);
+
+    // ================= Errors ==========================
+
+    error UserNotApproved();
+    error LenderNotApproved();
+    error CreditScoreAlreadyExists();
+
+    function initialize(address owner) public initializer {
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
+        _grantRole(ADMIN_ROLE, owner);
+        PausableUpgradeable.__Pausable_init();
+        ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
+    }
+
+    /** 
+      @dev CreditScore ranges between 300 and 850
+    */
+    function createUser(address taker) external onlyRole(LENDER_ROLE) {
+        require(getActiveUser(taker) == false, CreditScoreAlreadyExists());
+        LenderUserCreditScore[msg.sender][taker] = 300;
+    }
+
+    function createPaymentPlan(
+        address taker,
+        uint256 amount,
+        uint256 time,
+        uint256 interest
+    ) external onlyRole(LENDER_ROLE) {
+        // checks if the taker has a credit score
+        // create a new payment plan for the taker
+        // set the amount, time, interest, paidDebt, debt, totalPaid
+    }
+
+    function updateCreditScore() internal {}
+
+    function getActiveNumberOfPaymentPlans(
+        address taker
+    ) public pure returns (uint256) {
+        // checks if the taker has a credit score
+        // returns the number of payment plans the taker has
+    }
+
+    function getTotalUnpaidDebt(address taker) public pure returns (uint256) {
+        // checks if the taker has a credit score
+        // returns the total unpaid debt of the taker
+    }
+
+    // =================== User Functions =======================
+    function approveLender(address lender) public {
+        // checks if the taker has a credit score
+        // checks if the lender has a credit score
+        // checks if the lender is not already approved
+        // approves the lender
+    }
+
+    //==================== Lender Functions ====================
+
+    function payLoanPlusInterest(uint256 amount) public {
+        // checks if the taker has payed within the paymentplan give + score else - score
+        // if the whole debt is paid, the payment plan is set to inactive
+    }
+
+    // ================= Admin Functions ====================
+    function pause() external onlyRole(ADMIN_ROLE) {
+        _pause();
+    }
+
+    function unpause() external onlyRole(ADMIN_ROLE) {
+        _unpause();
+    }
+
+    function addLender(address lender) external onlyRole(ADMIN_ROLE) {
+        grantRole(LENDER_ROLE, lender);
+        activeLenders.push(lender);
+        emit LenderAdded(lender);
+    }
+
+    /** @dev If there is to many lenders this function no longer works because of hitting
+     * the Gas limit of a single transaction.
+     * if that is the case, we need to upgrade this contract and remove the foor loop.
+     */
+    function removeLender(address lender) external onlyRole(ADMIN_ROLE) {
+        revokeRole(LENDER_ROLE, lender);
+        for (uint256 i = 0; i < activeLenders.length; i++) {
+            if (activeLenders[i] == lender) {
+                activeLenders[i] = activeLenders[activeLenders.length - 1];
+                activeLenders.pop();
+                break;
+            }
+        }
+        emit LenderRemoved(lender);
+    }
+
+    /** @dev If there is to many lenders this function no longer works because of hitting
+     * the Gas limit of a single transaction.
+     * if that is the case, we need to upgrade this contract and remove the foor loop
+     */
+    function updateLender(
+        address oldLender,
+        address newLender
+    ) external onlyRole(ADMIN_ROLE) {
+        revokeRole(LENDER_ROLE, oldLender);
+        grantRole(LENDER_ROLE, newLender);
+        for (uint256 i = 0; i < activeLenders.length; i++) {
+            if (activeLenders[i] == oldLender) {
+                activeLenders[i] = newLender;
+                break;
+            }
+        }
+        emit LenderUpdated(oldLender, newLender);
+    }
+
+    // ================= Getter Functions ======================
+    function getActiveUser(
+        address taker
+    ) public view onlyRole(LENDER_ROLE) returns (bool) {
+        address lender = msg.sender;
+        bool active = (LenderUserCreditScore[lender][taker] != 0);
+        return active;
+    }
+
+    function getUserCreditScore(
+        address taker
+    ) public view returns (uint256 score) {
+        score = LenderUserCreditScore[msg.sender][taker];
+    }
+
+    function meanCreditScore(address taker) public view returns (uint256) {
+        return userMeanCreditScores[taker];
+    }
+
+    // ================= Internal Functions ====================
+}
--- contracts/test/testCreditScore.t.sol
+++ contracts/test/testCreditScore.t.sol
@@ -6,9 +6,39 @@ import {Test, console} from "forge-std/Test.sol";
 import {CreditScore} from "src/CreditScore.sol";
 
 contract TestCreditScore is Test {
-    CreditScore deployer;
+    CreditScore scoreKeeper;
+
+    address public owner = vm.addr(0x100);
+    address public lender = vm.addr(0x101);
+    address public alice = vm.addr(0x102);
+    address public bob = vm.addr(0x103);
 
     function setUp() public {
-        deployer = new CreditScore();
+        // Deploy the CreditScore contract and initialize it with the owner
+        scoreKeeper = new CreditScore();
+        scoreKeeper.initialize(owner);
+
+        // Grant Roles
+        vm.prank(owner);
+        scoreKeeper.addLender(lender);
+    }
+
+    function test_AddLender() public {
+        vm.startPrank(owner);
+        address newLender = vm.addr(0x104);
+        scoreKeeper.addLender(newLender);
+        vm.stopPrank();
+        assertTrue(scoreKeeper.hasRole(scoreKeeper.LENDER_ROLE(), newLender));
+    }
+
+    function test_NewCreateUser() public {
+        vm.startPrank(lender);
+        scoreKeeper.createUser(alice);
+        bool userIsActive = scoreKeeper.getActiveUser(alice);
+        uint256 score = scoreKeeper.getUserCreditScore(alice);
+        vm.stopPrank();
+
+        assertTrue(userIsActive);
+        assertEq(score, 300);
     }
 }
